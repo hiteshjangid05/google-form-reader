2acf152a53bda06b8a6298f58feeed49141edbbc
hiteshjangid05 <jangid.hitesh2112@gmail.com>
2025-08-26 20:57:19 +0530

    updated the link

--- src/utils/fetchResponses.js
+++ src/utils/fetchResponses.js
@@ -1,7 +1,7 @@
 import Papa from "papaparse";
 
-const CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQQDq0KCvsfUyhg5cpmcJ9KIkRthoDhi95W1k_37Cnid5Ww6A09ZEcJXJkqaMJXCvoiugEWuR22AfDv/pub?gid=1581606071&single=true&output=csv";
-
+// const CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQQDq0KCvsfUyhg5cpmcJ9KIkRthoDhi95W1k_37Cnid5Ww6A09ZEcJXJkqaMJXCvoiugEWuR22AfDv/pub?gid=1581606071&single=true&output=csv";
+const CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQQDq0KCvsfUyhg5cpmcJ9KIkRthoDhi95W1k_37Cnid5Ww6A09ZEcJXJkqaMJXCvoiugEWuR22AfDv/pub?gid=1581606071&single=true&output=csv"
 export const fetchResponses = async () => {
   const response = await fetch(CSV_URL);
   const csvText = await response.text();
